a04de2160c0e2c368fc646e1d07b5b2c4646ccdb
sajan <sajanloveu123@gmail.com>
2019-04-11 13:00:54 +0530

    add reset and card commponent

---
+++ src/Card.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Card extends React.Component {
+	
+	render {
+		return (
+			<div className="main-card-section">
+				<div>
+					<span></span>
+					<span></span>
+					<span></span>
+				</div>
+				<div>
+					<div>
+						<h2></h2>
+						<div><img src="" alt="avatar_img"/> <span>username</span></div>
+					</div>
+					<p className="issue-details"></p>
+				<div>
+			</div>
+		)
+	}
+}
