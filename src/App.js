700eb402643226cd4a73ee12aba75eab99e66e40
sajan <sajanloveu123@gmail.com>
2019-04-12 10:11:56 +0530

    new component added

--- src/App.js
+++ src/App.js
@@ -8,25 +8,31 @@ class App extends Component {
 		value: '',
 		page: 1,
 	}
+
 	handleInputValue =(e) => {
 		this.setState({value: e.target.value})
 	}
+
 	componentDidMount() {
 		fetch(`https://api.github.com/repos/google/googletest/issues?page=${this.state.page}`).then(res => res.json()).then(data => this.props.dispatch({type: 'ADD_DATA', payload: data}))
 	}
+
 	handleClick =() => {
 		fetch(`https://api.github.com/repos/${this.state.value}/issues?page=${this.state.page}`).then(res => res.json()).then(data => this.props.dispatch({type: 'ADD_QUERY', payload: data}))
 	}
+
 	incrementPage = () => {
 		this.setState({page: this.state.page++});
 		this.handleClick();
 		console.log(this.state.page ,'increment')
 	}
+
 	handleEnter = (e) => {
 		if(e.key === 'Enter') {
 			this.handleClick()
 		}
 	}
+
   render() {
   	console.log(this.props);
     return (
